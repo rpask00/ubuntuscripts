b03c3e90d1d59b579d5e2afb05267e03d6bc208b
Rafał Paśko <r.pasko@i-bs.pl>
2023-02-12 14:50:51 +0100

    12.02.2023 14:50

--- restore_configs.sh
+++ restore_configs.sh
@@ -5,9 +5,10 @@ config_locations="config_locations.txt"
 
 # Kopiowanie plików
 while read -r pair; do
-  source_file="${pair%% *}"
-  target_file="${pair#* }"
+  source_file="${pair#* }"
+  target_file="${pair%% *}"
   cp "$source_file" "$target_file"
   echo "Skopiowano plik $source_file do $target_file"
   echo ""
 done < "$config_locations"
+
