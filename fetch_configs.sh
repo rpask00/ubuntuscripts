a9d3a991b0943a26e13ad3e2ffbe7a21d5dc8a1c
Rafał Paśko <r.pasko@i-bs.pl>
2023-02-12 13:07:26 +0100

    12.02.2023 13:07

--- fetch_configs.sh
+++ fetch_configs.sh
@@ -5,3 +5,5 @@ cp ~/.ideavimrc ~/.config/scripts/configs/.ideavimrc
 cp ~/.vimrc ~/.config/scripts/configs/.vimrc
 cp ~/.config/compton.conf ~/.config/scripts/configs/compton.conf
 cp ~/.config/polybar/config ~/.config/scripts/configs/polybar_config
+cp ~/.zshrc ~/.config/scripts/config/.zshrc
+cp ~/.bashrc ~/.config/scripts/config/.bashrc
