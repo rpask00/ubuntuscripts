a692e2dbaa240bac8256d38e6240db67c2ce13a8
Rafał Paśko <r.pasko@i-bs.pl>
2023-02-12 14:55:28 +0100

    12.02.2023 14:55

--- fetch_configs.sh
+++ fetch_configs.sh
@@ -8,6 +8,8 @@ while read -r pair; do
   source_file="${pair%% *}"
   target_file="${pair#* }"
   cp "$source_file" "$target_file"
+  file_pairs_file=$(eval echo "$file_pairs_file")
+  echo "$file_pairs_file"
   echo "Skopiowano plik $source_file do $target_file"
   echo ""
 done < "$config_locations"
