90612a642f200c5101aa44c3780bfc288c606427
Rafał Paśko <r.pasko@i-bs.pl>
2023-03-01 21:53:11 +0100

    01.03.2023 21:53

---
+++ configs/polybar/modules/change_mic_volume.py
@@ -0,0 +1,11 @@
+import subprocess
+import argparse
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('arg1')
+args = parser.parse_args()
+mic_volumne = args.arg1
+
+
+subprocess.check_output(["amixer", "-c", "1", "set", "Mic", mic_volumne]).decode()
--- configs/polybar/modules/toggle_mic_volumne.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-from mic import get_mic_volume
-
-moc_volumne = get_mic_volume()
-
-if moc_volumne == '0%':
-    subprocess.check_output(["amixer", "-c", "1", "set", "Mic", "60"]).decode()
-else:
-    subprocess.check_output(["amixer", "-c", "1", "set", "Mic", "0"]).decode()
