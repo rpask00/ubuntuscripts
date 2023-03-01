783ce86a74dee9578af9d83c6cfa607ab47bf8d5
Rafał Paśko <r.pasko@i-bs.pl>
2023-03-01 09:23:31 +0100

    01.03.2023 09:23

---
+++ configs/polybar/modules/mic.py
@@ -0,0 +1,20 @@
+import subprocess
+import re
+
+
+# Uruchamia polecenie amixer -c 1 i przechwytuje wynik jako zmienną tekstową
+
+def get_mic_volume():
+    output = subprocess.check_output(["amixer", "-c", "1"]).decode()
+    output = output.split('\n')[-2]
+
+    pattern = r'\[(.*?)\]'
+    match = re.search(pattern, output)
+
+    if match:
+        return match.group(1).strip()
+    else:
+        return "---"
+
+
+print(get_mic_volume())
