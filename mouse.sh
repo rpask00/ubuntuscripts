2228b7ca0c29ab3c74a4e22aecf7436057420c6f
Rafał Paśko <r.pasko@i-bs.pl>
2023-03-20 08:55:53 +0100

    20.03.2023 08:55

--- mouse.sh
+++ mouse.sh
@@ -1,3 +1,5 @@
-for id in $(xinput list | grep "pointer" | cut -d '=' -f 2 | cut -f 1); do xinput --set-prop $id 'libinput Accel Profile Enabled' 0, 1; done
-xinput --set-prop "Wings Tech Gaming Mouse" "libinput Accel Speed" -0.2
+for id in $(xinput list | grep "pointer" | cut -d '=' -f 2 | cut -f 1); do 
+	xinput --set-prop $id 'libinput Accel Profile Enabled' 0, 1; 
+	xinput --set-prop $id "libinput Accel Speed" -0.2;
+done
 
