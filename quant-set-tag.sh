c4a4d4a4fa3da4754f1c2ccb44b9bbe76a61dca3
Rafal Pasko <rp.261000@gmail.com>
2023-07-28 09:33:45 +0200

    stash changes before checkout

--- quant-set-tag.sh
+++ quant-set-tag.sh
@@ -2,10 +2,30 @@
 
 cd ~/dev/quant-www
 
+previous_branch=$(git branch | grep \* | cut -d ' ' -f2)
+pending_changes=$(git status --porcelain)
+
 branch="develop"
 prefix="dev"
 
 skip_checkout="f"
+
+# Funkcja do stashingu i przywracania zmian
+stash_and_pop_changes() {
+    local action=$1
+
+    if [ "$action" == "pop" ]; then
+        git checkout $previous_branch
+    fi
+
+    if [ -n "$pending_changes" ]; then
+        if [ "$action" == "stash" ]; then
+            git stash push -u -m "Stashed changes before creating a new tag"
+        elif [ "$action" == "pop" ]; then
+            git stash pop
+        fi
+    fi
+}
 
 while getopts 'drts' OPTION; do
   case "$OPTION" in
@@ -24,11 +44,14 @@ while getopts 'drts' OPTION; do
   esac
 done
 
+# Stash zmian przed przełączeniem na inny branch
+stash_and_pop_changes "stash"
+
 if [ "$skip_checkout" == 'f' ]; then 
-    git checkout "$branch" || { echo "Błąd: Nie można przełączyć na branch $branch"; exit 1; }
-    git pull || { echo "Błąd: Nie można pobrać najnowszego kodu z brancha $branch"; exit 1; }
+    git checkout "$branch" || { echo "Błąd: Nie można przełączyć na branch $branch"; stash_and_pop_changes "pop"; exit 1; }
+    git pull || { echo "Błąd: Nie można pobrać najnowszego kodu z brancha $branch"; stash_and_pop_changes "pop"; exit 1; }
 fi
-    
+
 get_next_tag_number() {
     local pref=$1
     local date_today=$2
@@ -43,7 +66,6 @@ get_next_tag_number() {
     fi
 }
 
-
 today=$(date +"%d-%m-%Y")
 next_number=$(get_next_tag_number "$prefix" "$today")
 new_tag="$prefix/$today/$next_number"
@@ -55,12 +77,14 @@ if [ "$confirm" == "t" ]; then
     # Sprawdź, czy tag już nie istnieje
     if git rev-parse "$new_tag" >/dev/null 2>&1; then
         echo "Błąd: Tag $new_tag już istnieje. Nie można go ponownie utworzyć."
+        stash_and_pop_changes "pop"
         exit 1
     fi
 
     # Dodaj nowy tag na aktualnym commicie
     git tag "$new_tag"
     if [ $? -eq 0 ]; then
+        # Odkomentuj poniższą linię, aby automatycznie przesłać nowy tag do zdalnego repozytorium
         git push origin "$new_tag"
         echo "Utworzono nowy tag: $new_tag"
     else
@@ -69,3 +93,10 @@ if [ "$confirm" == "t" ]; then
 else
     echo "Anulowano tworzenie tagu."
 fi
+
+
+
+git checkout $previous_branch
+
+# Przywróć zmiany ze stash'a po zakończeniu skryptu
+stash_and_pop_changes "pop"
