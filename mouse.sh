9b1f64a9a1601b718a24cec692c54018cbce63a2
Rafal Pasko <rp.261000@gmail.com>
2025-08-11 12:48:03 +0200

    11.08.2025 12:48

--- mouse.sh
+++ mouse.sh
@@ -1,5 +1,5 @@
-for id in $(xinput list | grep "pointer" | cut -d '=' -f 2 | cut -f 1); do 
+for id in $(xinput list | grep "Mouse" | cut -d '=' -f 2 | cut -f 1); do 
 	xinput --set-prop $id 'libinput Accel Profile Enabled' 0, 1; 
-	xinput --set-prop $id "libinput Accel Speed" -0;
+	xinput --set-prop $id "libinput Accel Speed" -0.2;
 done
 
